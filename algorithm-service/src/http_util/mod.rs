d890318f0350264fc953aa019a0f7ee9c0190314
mark <mark@101.inc>
2024-02-25 22:17:13 +0900

    http-module: http 중 get, post 를 담당하는 util 을 생성한다

---
+++ algorithm-service/src/http_util/mod.rs
@@ -0,0 +1,170 @@
+use reqwest::blocking;
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, error::Error, io::Read};
+
+pub struct HttpClient {
+    request_host: RequestHost,
+}
+
+pub struct RequestHost {
+    protocol: String,
+    host: String,
+    port: i128,
+}
+
+impl RequestHost {
+    fn get_base_url(&self) -> String {
+        return format!("{}://{}:{}", &self.protocol, &self.host, &self.port);
+    }
+}
+
+pub enum HttpMethod {
+    GET,
+    POST,
+    PATCH,
+    PUT,
+}
+
+pub struct RequestHttp {
+    uri: String,
+    method: HttpMethod,
+    body: Option<String>,
+}
+
+
+impl HttpClient {
+    fn get<T>(&self, path: &str, path_param: Option<Vec<(String, String)>>) -> Option<T>
+    where
+        T: Serialize + for<'a> Deserialize<'a>,
+    {
+        let str_path = match path_param {
+            Some(params) => generate_path(path, &params),
+            None => path.to_string(),
+        };
+
+        let result = RequestHttp::new(HttpMethod::GET,
+            &self.request_host, 
+            Some(&str_path),
+            None
+        );
+
+        if let Ok(item) = result.get_with_http() {
+            return Some(serde_json::from_str(&item).unwrap());
+        }
+        return None;
+    }
+
+
+    fn post<T, R>(&self, path: &str, path_param: Option<Vec<(String, String)>>,body: T) -> Option<R>
+    where
+        T: Serialize,
+        R: for<'a> Deserialize<'a>,
+    {
+
+        let str_path = match path_param {
+            Some(params) => generate_path(path, &params),
+            None => path.to_string(),
+        };
+
+        let result = RequestHttp::new(HttpMethod::GET,
+            &self.request_host, 
+            Some(&str_path),
+            Some(&serde_json::to_string(&body).ok()?)
+        );
+        return Some(serde_json::from_str(&result.post_with_http().unwrap()).expect("Parsing Exception"));
+    }
+}
+
+impl RequestHttp {
+    pub fn new(
+        method: HttpMethod,
+        host: &RequestHost,
+        path: Option<&String>,
+        body: Option<&String>,
+    ) -> RequestHttp {
+        return RequestHttp {
+            uri: match path {
+                Some(item) => {
+                    format!("{}/{}", host.get_base_url(), item)
+                }
+                None => host.get_base_url(),
+            },
+            method,
+            body: match body {
+                Some(item) => Some(item.clone()),
+                None => None,
+            },
+        };
+    }
+
+    pub fn post_with_http(&self) -> Result<String, Box<dyn Error>> {
+        let client = blocking::Client::new();
+        let rsp = client.post(&self.uri)
+        .body(match &self.body {
+            Some(item) => {
+                item.clone()
+            }
+            None => "".to_string()
+               })
+        .send()
+        .expect(&format!("Fail To Send Message {}", &self.uri));
+
+        Ok(rsp.text().expect("parsing exception response to text"))
+    }
+
+    pub fn get_with_http(&self) -> Result<String, Box<dyn Error>> {
+        Ok(blocking::get(&self.uri)?.text().unwrap())
+    }
+}
+
+pub fn generate_path(path: &str, path_param: &Vec<(String, String)>) -> String {
+    let mut replace_path: String = path.to_string();
+    for (key, value) in path_param {
+        replace_path = replace_path.replace(&("{".to_string() + key + &"}"), value);
+    }
+
+    replace_path
+}
+
+#[cfg(test)]
+mod test_http {
+    use super::*;
+    use serde::{Deserialize, Serialize};
+
+    #[derive(Serialize, Deserialize, Debug)]
+
+    struct JsonPlaceHolderData {
+        userId: i32,
+        id: i32,
+        title: String,
+        completed: bool,
+    }
+
+    #[test]
+    fn test_request() {
+        let client = HttpClient {
+            request_host: RequestHost {
+                protocol: "https".to_string(),
+                host: "jsonplaceholder.typicode.com".to_string(),
+                port: 443,
+            },
+        };
+
+        let result: Option<JsonPlaceHolderData> = client.get("/todos/1", None);
+        println!("{:?}", result);
+    }
+
+    #[test]
+    fn test_generate_path() {
+        println!(
+            "{}",
+            generate_path(
+                "/users/{userId}/klassId/{klassId}",
+                &vec![
+                    ("userId".to_string(), "t1".to_string()),
+                    ("klassId".to_string(), "t2".to_string())
+                ]
+            )
+        );
+    }
+}
--- algorithm-service/src/main.rs
+++ algorithm-service/src/main.rs
@@ -1,4 +1,6 @@
 use algorithm;
+mod http_util;
+
 fn main() {
     let mut vector = vec![1,6,45,2];
     algorithm::bubble_sort(&mut vector);
