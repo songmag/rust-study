12cfe1d7d9d89914f8c8b8b415de951365d16712
mark <mark@101.inc>
2024-02-28 23:50:39 +0900

    채팅의 Context 를 유지하도록 변경한다

--- algorithm-service/src/gemma/chat_tool.rs
+++ algorithm-service/src/gemma/chat_tool.rs
@@ -7,14 +7,15 @@ use super::GeminiAPIService;
 
 pub struct ChatToolGemini {
     gemma_api : GemmaAPI,
-    prompt: String,
+    prompt: String
 }
 
 impl ChatToolGemini {
     pub fn new(api : GemmaAPI) -> ChatToolGemini {
+        api.talk_to_gemma_with_text("너의 모든 대화 응답은 [ 와 ] 로 감싸져 있어 내가 하는 말은 [] 로 안 감싸져 있는 글자야, 그리고 이전 대화 내용은 ___로 시작하고 ___로 끝나".to_string());
         ChatToolGemini {
             gemma_api : api,
-            prompt: String::from("")
+            prompt: String::from("너의 모든 대화 응답은 [ 와 ] 로 감싸져 있어 내가 하는 말은 [] 로 안 감싸져 있는 글자야, 그리고 이전 대화 내용은 ___로 시작하고 ___로 끝나")
         }
     }
 }
@@ -25,7 +26,7 @@ impl GeminiAPIService for ChatToolGemini {
     }
 
     fn send(&self, text: &String) -> Option<GemmaResponse> {
-        self.gemma_api.talk_to_gemma_with_text(format!("{}\n{}", self.prompt, text))
+        self.gemma_api.talk_to_gemma_with_text(format!("___{}___\n{}", self.prompt, text))
     }
 }
 
